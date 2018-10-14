7571d905cc1f52db64ff055e9b53282fab829988
Jeffrey Polasz <jeff_polasz@hotmail.com>
2018-10-14 18:33:05 -0400

    Added A* algorithm with visuals

--- Assets/AStarGrid.cs
+++ Assets/AStarGrid.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class AStarGrid : MonoBehaviour {
     public LayerMask unwalkableMask;
@@ -25,9 +26,29 @@ public class AStarGrid : MonoBehaviour {
             for (int y = 0; y < gridSizeY; y++) {
                 Vector3 worldPoint = worldBottomLeft + Vector3.right * (x * nodeDiameter + nodeRadius) + Vector3.forward * (y * nodeDiameter + nodeRadius);
                 bool walkable = !(Physics.CheckSphere(worldPoint, nodeRadius, unwalkableMask));
-                grid[x, y] = new Node(walkable, worldPoint);
+                grid[x, y] = new Node(walkable, worldPoint, x, y);
             }
         }
+    }
+
+    public List<Node> GetNeighbours(Node node) {
+        List<Node> neighbours = new List<Node>();
+
+        for (int x = -1; x <= 1; x++) {
+            for (int y = -1; y <= 1; y++) {
+                if (x == 0 && y == 0) {
+                    continue;
+                }
+
+                int checkX = node.gridX + x;
+                int checkY = node.gridY + y;
+
+                if (checkX >= 0 && checkX < gridSizeX && checkY >= 0 && checkY < gridSizeY) {
+                    neighbours.Add(grid[checkX, checkY]);
+                }
+            }
+        }
+        return neighbours;
     }
 
     public Node NodeFromWorldPoint(Vector3 worldPosition) {
@@ -41,12 +62,18 @@ public class AStarGrid : MonoBehaviour {
         return grid[x, y];
     }
 
+    public List<Node> path;
     void OnDrawGizmos() {
         Gizmos.DrawWireCube(transform.position, new Vector3(gridWorldSize.x, 1, gridWorldSize.y));
 
         if (grid != null) {
             foreach (Node n in grid) {
                 Gizmos.color = (n.walkable) ? Color.white : Color.red;
+                if (path != null) {
+                    if (path.Contains(n)) {
+                        Gizmos.color = Color.black;
+                    }
+                }
                 Gizmos.DrawCube(n.worldPosition, Vector3.one * (nodeDiameter - 0.1f));
             }
         }
@@ -56,9 +83,22 @@ public class AStarGrid : MonoBehaviour {
 public class Node {
     public bool walkable;
     public Vector3 worldPosition;
+    public int gridX;
+    public int gridY;
 
-    public Node(bool _walkable, Vector3 _worldPos) {
+    public int gCost;
+    public int hCost;
+    public Node parent;
+
+    public Node(bool _walkable, Vector3 _worldPos, int _gridX, int _gridY) {
         walkable = _walkable;
         worldPosition = _worldPos;
+        gridX = _gridX;
+        gridY = _gridY;
     }
+
+    public int fCost {
+        get { return gCost + hCost; }
+    }
+
 } // Node
---
+++ Assets/Pathfinding.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinding : MonoBehaviour {
+    public Transform seeker, target;
+
+    AStarGrid grid;
+
+    void Awake() {
+        grid = GetComponent<AStarGrid>();
+    }
+
+    void Update() {
+        FindPath(seeker.position, target.position);
+    }
+
+    void FindPath(Vector3 startPos, Vector3 targetPos) {
+        Node startNode = grid.NodeFromWorldPoint(startPos);
+        Node targetNode = grid.NodeFromWorldPoint(targetPos);
+
+        List<Node> openSet = new List<Node>(); // Set of nodes to be evaluated
+        HashSet<Node> closedSet = new HashSet<Node>(); // Set of nodes already evaluated
+        openSet.Add(startNode);
+
+        while (openSet.Count > 0) { // Loop until empty
+            Node node = openSet[0];
+            for (int i = 1; i < openSet.Count; i++) {
+                if (openSet[i].fCost < node.fCost || openSet[i].fCost == node.fCost) { // Unoptimized for now
+                    if (openSet[i].hCost < node.hCost) {
+                        node = openSet[i];
+                    }
+                }
+            }
+
+            openSet.Remove(node);
+            closedSet.Add(node);
+
+            if (node == targetNode) { // Found path
+                RetracePath(startNode, targetNode);
+                return;
+            }
+
+            foreach (Node neighbour in grid.GetNeighbours(node)) {
+                if (!neighbour.walkable || closedSet.Contains(neighbour)) {
+                    continue;
+                }
+
+                int newMovementCostToNeighbour = node.gCost + GetDistance(node, neighbour);
+                if (newMovementCostToNeighbour < neighbour.gCost || !openSet.Contains(neighbour)) {
+                    neighbour.gCost = newMovementCostToNeighbour;
+                    neighbour.hCost = GetDistance(neighbour, targetNode);
+                    neighbour.parent = node;
+
+                    if (!openSet.Contains(neighbour)) {
+                        openSet.Add(neighbour);
+                    }
+                }
+            }
+        }
+    } // FindPath
+
+    void RetracePath(Node startNode, Node endNode) {
+        List<Node> path = new List<Node>();
+        Node currentNode = endNode;
+
+        while (currentNode != startNode) {
+            path.Add(currentNode);
+            currentNode = currentNode.parent;
+        }
+        path.Reverse();
+
+        grid.path = path;
+    }
+
+    // Diagonal Shortcut Method
+    int GetDistance(Node nodeA, Node nodeB) {
+        int distX = Mathf.Abs(nodeA.gridX - nodeB.gridX);
+        int distY = Mathf.Abs(nodeA.gridY - nodeB.gridY);
+
+        if (distX > distY) {
+            return 14 * distY + 10 * (distX - distY);
+        } else {
+            return 14 * distX + 10 * (distY - distX);
+        }
+    }
+
+} // Pathfinding
